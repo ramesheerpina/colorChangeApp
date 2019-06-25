5783cc677a0edbea2a009dfa5de35d4f414a6487
Ramesh <ramesh.eerpina@gmail.com>
2019-06-24 21:33:57 -0400

    A simple welcome screen with text and image

--- App.js
+++ App.js
@@ -1,37 +1,63 @@
 import React, {Component} from 'react'
 import { 
+  Dimensions,
+  Image,
   StyleSheet,
   AppRegistry, 
   Text, 
   View,
   StatusBar} from 'react-native'
 
-const styles = StyleSheet.create ({
-  defaultText: {
-    fontSize: 22,
-    padding: 30,
-    color: 'red',
-    borderWidth: StyleSheet.hairlineWidth,
-    margin: 5
-  },
-  selectedText: {
-    backgroundColor: 'yellow',
-    color: 'blue',
-    fontWeight: 'bold'
-  }
-})
+  import plane from './plane.jpg'
+
 export default class App extends React.Component {
   render () {
     return (
-      <View>
+      <View style = {styles.container}>
         <StatusBar hidden = {true}></StatusBar>
-        <Text style={styles.defaultText}>Welcome Aboard
+        <Text style={[styles.defaultText]}>Welcome Aboard
         </Text>
-        <Text style={[styles.selectedText,styles.defaultText]}>Air Canada
+        <Text style={[styles.selectedText]}>Air Canada
         </Text>
+        <View>
+          <Image style = {styles.pic} source = {plane}>
+          </Image>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  pic : {
+    backgroundColor: 'white',
+    borderRadius: 100,
+    width: Dimensions.get('window').width, 
+    height: 160,
+    resizeMode: 'cover',
+    
+  },
+  defaultText: {
+    fontSize: 22,
+    padding: 30,
+    color: 'red',
+    margin: 5
+  },
+  selectedText: {
+    backgroundColor: 'black',
+    fontSize: 22,
+    justifyContent: 'center',
+    padding: 20,
+    paddingLeft: 141,
+    paddingRight: 141,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+})
 
 AppRegistry.registerComponent('helloworld', () => App)
