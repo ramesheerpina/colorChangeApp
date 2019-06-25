a102cb73933d16c527e01e960d7ff1e173e54794
Ramesh <ramesh.eerpina@gmail.com>
2019-06-25 10:43:54 -0400

    adding multiple colors with ScrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { View, StyleSheet} from 'react-native'
+import { ScrollView, StyleSheet} from 'react-native'
 import ColorButton from './components/colorButton'
 
   import plane from './plane.jpg'
@@ -21,12 +21,20 @@ export default class App extends React.Component {
     const {backgroundColor} = this.state
     return (
       //getting the background color from state that was declared in constructor above
-      <View style = {[styles.container, {backgroundColor}]}>
+      <ScrollView style = {[styles.container, {backgroundColor}]}>
           <ColorButton backgroundColor = 'red' onSelect= {this.changeColor}/>
           <ColorButton backgroundColor = 'green' onSelect= {this.changeColor}/>
           <ColorButton backgroundColor = 'blue' onSelect= {this.changeColor}/>
           <ColorButton backgroundColor = 'salmon' onSelect= {this.changeColor}/>
-      </View>
+          <ColorButton backgroundColor = '#00ff00' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'rgb(255,0,255)' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'pink' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'yellow' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'indigo' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'violet' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'orange' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'rgb(255,200,255)' onSelect= {this.changeColor}/>
+      </ScrollView>
     )
   }
 }
@@ -35,7 +43,6 @@ const styles = StyleSheet.create (
   {
   container : {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    paddingTop: 20
   }
 })
