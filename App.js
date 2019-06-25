61892f2ad5ed74b184a379f56b7ea7323e979e6d
Ramesh <ramesh.eerpina@gmail.com>
2019-06-25 10:16:25 -0400

    create ColorButton as comp and add to the main app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-import {Text, View, StyleSheet, TouchableHighlight} from 'react-native'
+import { View, StyleSheet} from 'react-native'
+import ColorButton from './components/colorButton'
 
   import plane from './plane.jpg'
 
@@ -21,13 +22,10 @@ export default class App extends React.Component {
     return (
       //getting the background color from state that was declared in constructor above
       <View style = {[styles.container, {backgroundColor}]}>
-        <TouchableHighlight style= {styles.button} onPress = {() => this.changeColor('yellow')} underlayColor="orange">
-        <View style = {styles.row}>
-          <View style = {[styles.sample, {backgroundColor:'yellow'}]}/>
-            <Text style = {styles.text}>yellow</Text>
-        </View>
-        </TouchableHighlight>
-
+          <ColorButton backgroundColor = 'red' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'green' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'blue' onSelect= {this.changeColor}/>
+          <ColorButton backgroundColor = 'salmon' onSelect= {this.changeColor}/>
       </View>
     )
   }
@@ -39,30 +37,5 @@ const styles = StyleSheet.create (
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  button : {
-    
-    padding : 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignSelf: 'stretch',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)'
-    
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  sample : {
-    height : 20,
-    width: 20,
-    borderRadius: 10,
-    margin : 5,
-    backgroundColor: 'white',
-  },
-  text: {
-    fontSize : 30,
-    margin : 5
   }
 })
