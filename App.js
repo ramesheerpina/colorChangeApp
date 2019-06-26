0d3f0afa62a72152c83ce2f111383550647f6223
Ramesh <ramesh.eerpina@gmail.com>
2019-06-26 10:28:01 -0400

    add ColorList as a component and display an alert

--- App.js
+++ App.js
@@ -1,71 +1,11 @@
 import React, {Component} from 'react'
-import { ListView, StyleSheet, Text} from 'react-native'
-import ColorButton from './components/colorButton'
-import ColorForm from './components/ColorForm'
-
-  import plane from './plane.jpg'
+import { Alert} from 'react-native'
+import ColorList from './components/ColorList'
 
 export default class App extends React.Component {
-  constructor () {
-    super()
-    this.ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1!==r2
-    })
-    const availableColors = [
-      'Red',
-      'Green',
-      'Yellow'
-    ]
-    this.state = {
-      backgroundColor: 'blue', availableColors, dataSource: this.ds.cloneWithRows(availableColors)
-    }
-    //this is used to set state and change the color within that state
-    this.changeColor = this.changeColor.bind(this)
-    this.newColor = this.newColor.bind(this)
-  }
-  //declaring a method to change the backgroundcolor
-  changeColor(backgroundColor) {
-    this.setState({backgroundColor})
-  }
-
-  newColor(color) {
-    const availableColors = [
-      ...this.state.availableColors, color
-    ]
-    this.setState({
-      availableColors, dataSource: this.ds.cloneWithRows(availableColors)
-    })
-  }
-
   render () {
-    const {backgroundColor, dataSource} = this.state
     return (
-      //getting the background color from state that was declared in constructor above
-      <ListView style = {[styles.container, {backgroundColor}]}
-      dataSource={dataSource} 
-      renderRow= {(color) => (<ColorButton backgroundColor = {color} onSelect= {this.changeColor}/>
-        )}
-      renderHeader = {() => (
-        <ColorForm onNewColor = {this.newColor}/>
-      )}>
-      
-        
-      </ListView>
+      <ColorList onColorSelected = { color => Alert.alert(color)} />
     )
   }
 }
-
-const styles = StyleSheet.create (
-  {
-  container : {
-    flex: 1
-    
-  },
-  header: {
-    backgroundColor : 'lightgrey',
-    paddingTop: 20,
-    padding: 10,
-    fontSize: 30,
-    textAlign: 'center'
-  }
-})
---
+++ components/ColorList.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react'
+import { ListView, StyleSheet, AsyncStorage} from 'react-native'
+import PropTypes from 'react'
+import ColorButton from './colorButton'
+import ColorForm from './ColorForm'
+
+
+export default class ColorList extends Component {
+  constructor () {
+    super()
+    this.ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1!==r2
+    })
+    const availableColors = []
+    this.state = {
+       availableColors, dataSource: this.ds.cloneWithRows(availableColors)
+    }
+    //this is used to set state and change the color within that state
+
+    this.newColor = this.newColor.bind(this)
+  }
+  componentDidMount () {
+    AsyncStorage.getItem(
+      '@ColorListStore:Colors',
+      (err, data)=> {
+        if (err) {
+          console.error('Error loading colors', err)
+        } else {
+          const availableColors = JSON.parse(data)
+          this.setState({
+            availableColors,
+            dataSource : this.ds.cloneWithRows(availableColors)
+          })
+        }
+      } 
+    )
+  }
+
+  saveColors(colors) {
+    AsyncStorage.setItem(
+      '@ColorListStore:Colors',
+      JSON.stringify(colors)
+    )
+  }
+
+  newColor(color) {
+    const availableColors = [
+      ...this.state.availableColors, color
+    ]
+    this.setState({
+      availableColors, dataSource: this.ds.cloneWithRows(availableColors)
+    })
+    this.saveColors(availableColors)
+  }
+
+  render () {
+    const {backgroundColor, dataSource} = this.state
+    return (
+      //getting the background color from state that was declared in constructor above
+      <ListView style = {[styles.container, {backgroundColor}]}
+      dataSource={dataSource} 
+      renderRow= {(color) => (<ColorButton backgroundColor = {color} onSelect= {this.props.onColorSelected}/>
+        )}
+      renderHeader = {() => (
+        <ColorForm onNewColor = {this.newColor}/>
+      )}> 
+      </ListView>
+    )
+  }
+}
+
+ColorList.defaultProps = {
+  onColorSelected: f => f
+}
+
+ColorList.PropTypes = {
+  onColorSelected: PropTypes.func
+}
+
+const styles = StyleSheet.create (
+  {
+  container : {
+    flex: 1
+    
+  },
+  header: {
+    backgroundColor : 'lightgrey',
+    paddingTop: 20,
+    padding: 10,
+    fontSize: 30,
+    textAlign: 'center'
+  }
+})
